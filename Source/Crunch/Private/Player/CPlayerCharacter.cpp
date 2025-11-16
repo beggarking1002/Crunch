0be086b79f6b8e6935e3e1901b61b98de09a8a7b
Beggarking1002 <drake1002@naver.com>
2025-11-16 13:29:52 +0900

    Basic Components of Player/Jump Input Action
    Basic Components of Player/Jump Input Action

--- Source/Crunch/Private/Player/CPlayerCharacter.cpp
+++ Source/Crunch/Private/Player/CPlayerCharacter.cpp
@@ -3,3 +3,42 @@
 
 #include "Player/CPlayerCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/PlayerController.h"
+
+ACPlayerCharacter::ACPlayerCharacter()
+{
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("Camera Boom");
+	CameraBoom-> SetupAttachment(GetRootComponent());
+
+	ViewCam = CreateDefaultSubobject<UCameraComponent>("View Cam");
+	ViewCam->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+}
+
+void ACPlayerCharacter::PawnClientRestart()
+{
+	Super::PawnClientRestart();
+	APlayerController* OwningPlayerController = GetController<APlayerController>();
+	if (OwningPlayerController)
+	{
+		UEnhancedInputLocalPlayerSubsystem* InputSubsystem = OwningPlayerController->GetLocalPlayer()->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+		if (InputSubsystem)
+		{
+			InputSubsystem->RemoveMappingContext(GameplayInputMappingContext);
+			InputSubsystem->AddMappingContext(GameplayInputMappingContext, 0);
+		}
+	}
+}
+
+void ACPlayerCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedInputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComp)
+	{
+		EnhancedInputComp->BindAction(JumpInputAction, ETriggerEvent::Triggered, this, &ACPlayerCharacter::Jump);
+	}
+}
