6650cce0db82eafea385217ad4a0dc5652b0a353
Beggarking1002 <drake1002@naver.com>
2025-12-02 19:18:32 +0900

    Add the behavior Tree and Blackboard to the AI and Update the Target
    Add the behavior Tree and Blackboard to the AI and Update the Target

--- Source/Crunch/Private/AI/CAIController.cpp
+++ Source/Crunch/Private/AI/CAIController.cpp
@@ -3,6 +3,7 @@
 
 #include "AI/CAIController.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
 
@@ -23,6 +24,7 @@ ACAIController::ACAIController()
 	SightConfig->PeripheralVisionAngleDegrees = 180.f;
 
 	AIPerceptionComponent->ConfigureSense(*SightConfig);
+	AIPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ACAIController::TargetPerceptionUpdated);
 }
 
 void ACAIController::OnPossess(APawn* NewPawn)
@@ -34,5 +36,53 @@ void ACAIController::OnPossess(APawn* NewPawn)
 	if (PawnTeamInterface)
 	{
 		PawnTeamInterface->SetGenericTeamId(GetGenericTeamId());
+	}
+}
+
+void ACAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	RunBehaviorTree(BehaviorTree);
+}
+
+void ACAIController::TargetPerceptionUpdated(AActor* TargetActor, FAIStimulus Stimulus)
+{
+	if (Stimulus.WasSuccessfullySensed())
+	{
+		if (!GetCurrentTarget())
+		{
+			SetCurrentTarget(TargetActor);
+		}
+	}
+	else
+	{
+		if (GetCurrentTarget() == TargetActor)
+			SetCurrentTarget(nullptr);
+	}
+}
+
+const UObject* ACAIController::GetCurrentTarget() const
+{
+	const UBlackboardComponent* BlackboardComponent = GetBlackboardComponent();
+	if (BlackboardComponent)
+	{
+		return GetBlackboardComponent()->GetValueAsObject(TargetBlackboardKeyName);
+	}
+	return nullptr;
+}
+
+void ACAIController::SetCurrentTarget(AActor* NewTarget)
+{
+	UBlackboardComponent* BlackboardComponent = GetBlackboardComponent();
+	if (!BlackboardComponent)
+		return;
+
+	if (NewTarget)
+	{
+		BlackboardComponent->SetValueAsObject(TargetBlackboardKeyName, NewTarget);
+	}
+	else
+	{
+		BlackboardComponent->ClearValue(TargetBlackboardKeyName);
 	}
 }
