217d7c4c035b4d8493cfcf778ba2806e15db5ce0
Beggarking1002 <drake1002@naver.com>
2025-12-07 22:28:00 +0900

    Adjust the Health Bar Display of the Minion
    Adjust the Health Bar Display of the Minion

--- Source/Crunch/Private/Widgets/ValueGauge.cpp
+++ Source/Crunch/Private/Widgets/ValueGauge.cpp
@@ -10,6 +10,10 @@ void UValueGauge::NativePreConstruct()
 {
 	Super::NativePreConstruct();
 	ProgressBar->SetFillColorAndOpacity(BarColor);
+
+	ValueText->SetFont(ValueTextFont);
+	ValueText->SetVisibility(bValueTextVisible ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+	ProgressBar->SetVisibility(bProgressBarVisible ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
 }
 
 void UValueGauge::SetAndBoundToGameplayAttribute(class UAbilitySystemComponent* AbilitySystemComponent,
