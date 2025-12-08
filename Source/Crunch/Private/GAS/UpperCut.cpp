b6e37dde84dcb861febb03f68de9cd9689daf439
Beggarking1002 <drake1002@naver.com>
2025-12-08 21:10:41 +0900

    Add Damage Cooldown Cost and Stun Effect to Upper Cut
    Add Damage Cooldown Cost and Stun Effect to Upper Cut

--- Source/Crunch/Private/GAS/UpperCut.cpp
+++ Source/Crunch/Private/GAS/UpperCut.cpp
@@ -44,6 +44,7 @@ void UUpperCut::StartLaunching(FGameplayEventData EventData)
 		for (FHitResult& HitResult : TargetHitResults)
 		{
 			PushTarget(HitResult.GetActor(), FVector::UpVector *UpperCutLaunchSpeed);
+			ApplyGameplayEffectToHitResultActor(HitResult, LaunchDamageEffect, GetAbilityLevel(CurrentSpecHandle, CurrentActorInfo));
 		}
 	}
 }
