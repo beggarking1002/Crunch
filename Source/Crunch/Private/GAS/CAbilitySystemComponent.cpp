f118da10e028226bc708ed033a4a0fa322f6037b
Beggarking1002 <drake1002@naver.com>
2025-12-16 12:58:55 +0900

    Add the Health and Mana Full and Empty Tags
    Add the Health and Mana Full and Empty Tags

--- Source/Crunch/Private/GAS/CAbilitySystemComponent.cpp
+++ Source/Crunch/Private/GAS/CAbilitySystemComponent.cpp
@@ -2,6 +2,8 @@
 
 
 #include "GAS/CAbilitySystemComponent.h"
+
+#include "CAbilitySystemStatics.h"
 #include "GAS/CGameplayAbilityTypes.h"
 #include "CAttributeSet.h"
 #include "CHeroAttributeSet.h"
@@ -9,6 +11,7 @@
 UCAbilitySystemComponent::UCAbilitySystemComponent()
 {
 	GetGameplayAttributeValueChangeDelegate(UCAttributeSet::GetHealthAttribute()).AddUObject(this, &UCAbilitySystemComponent::HealthUpdated);
+	GetGameplayAttributeValueChangeDelegate(UCAttributeSet::GetManaAttribute()).AddUObject(this, &UCAbilitySystemComponent::ManaUpdated);
 	GenericConfirmInputID = (int32)ECAbilityInputID::Confirm;
 	GenericCancelInputID = (int32)ECAbilityInputID::Cancel;
 }
@@ -102,9 +105,67 @@ void UCAbilitySystemComponent::AuthApplyGameplayEffect(TSubclassOf<UGameplayEffe
 
 void UCAbilitySystemComponent::HealthUpdated(const FOnAttributeChangeData& ChangeData)
 {
-	if (!GetOwner()) return;
-	if (ChangeData.NewValue <= 0 && GetOwner()->HasAuthority()&&DeathEffect)
+	if (!GetOwner() || !GetOwner()->HasAuthority()) return;
+	bool bFound = false;
+	float MaxHealth = GetGameplayAttributeValue(UCAttributeSet::GetMaxHealthAttribute(), bFound);
+	if (bFound && ChangeData.NewValue >= MaxHealth)
 	{
-		AuthApplyGameplayEffect(DeathEffect);
+		if (!HasMatchingGameplayTag(UCAbilitySystemStatics::GetHealthFullStatTag()))
+		{
+			//This is done local only.
+			AddLooseGameplayTag(UCAbilitySystemStatics::GetHealthFullStatTag());
+		}
+	}
+
+	else
+	{
+		RemoveLooseGameplayTag(UCAbilitySystemStatics::GetHealthFullStatTag());
+	}
+
+	if (ChangeData.NewValue <= 0)
+	{
+		if (!HasMatchingGameplayTag(UCAbilitySystemStatics::GetHealthEmptyStatTag()))
+		{
+			AddLooseGameplayTag(UCAbilitySystemStatics::GetHealthEmptyStatTag());
+
+			if(DeathEffect)
+				AuthApplyGameplayEffect(DeathEffect);
+		}
+	}
+	else
+	{
+		RemoveLooseGameplayTag(UCAbilitySystemStatics::GetHealthEmptyStatTag());
+	}
+}
+
+void UCAbilitySystemComponent::ManaUpdated(const FOnAttributeChangeData& ChangeData)
+{
+	if (!GetOwner() || !GetOwner()->HasAuthority()) return;
+
+	bool bFound = false;
+	float MaxMana = GetGameplayAttributeValue(UCAttributeSet::GetMaxManaAttribute(), bFound);
+	if (bFound && ChangeData.NewValue >= MaxMana)
+	{
+		if (!HasMatchingGameplayTag(UCAbilitySystemStatics::GetManaFullStatTag()))
+		{
+			//This is done local only.
+			AddLooseGameplayTag(UCAbilitySystemStatics::GetManaFullStatTag());
+		}
+	}
+	else
+	{
+		RemoveLooseGameplayTag(UCAbilitySystemStatics::GetManaFullStatTag());
+	}
+
+	if (ChangeData.NewValue <= 0)
+	{
+		if (!HasMatchingGameplayTag(UCAbilitySystemStatics::GetManaEmptyStatTag()))
+		{
+			AddLooseGameplayTag(UCAbilitySystemStatics::GetManaEmptyStatTag());
+		}
+	}
+	else
+	{
+		RemoveLooseGameplayTag(UCAbilitySystemStatics::GetManaEmptyStatTag());
 	}
 }
