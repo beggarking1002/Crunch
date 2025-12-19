411a4d88c9cc5942f25ec96fc25d4f0ae8bcbb10
Beggarking1002 <drake1002@naver.com>
2025-12-19 17:04:07 +0900

    Create Helper  Functions to Find Realtime Ability Cooldown and Cost
    Create Helper  Functions to Find Realtime Ability Cooldown and Cost
    Define the Ability Cooldown and Cost Effects on each Level with Curve Table

--- Source/Crunch/Private/Widgets/AbilityGauge.h
+++ Source/Crunch/Private/Widgets/AbilityGauge.h
@@ -105,5 +105,5 @@ private:
 	void AbilitySpecUpdated(const FGameplayAbilitySpec& AbilitySpec);
 	void UpdateCanCast();
 	void UpgradePointUpdated(const FOnAttributeChangeData& Data);
-	
+	void ManaUpdated(const FOnAttributeChangeData& Data);
 };
