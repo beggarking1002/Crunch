56363d927073f6bd58cfb826a66a3a0626819cdd
Beggarking1002 <drake1002@naver.com>
2025-12-19 18:15:32 +0900

    Refactor AN_SendTargetGroup to Send Damage GameplayCue Locally
    Refactor AN_SendTargetGroup to Send Damage GameplayCue Locally

--- Source/Crunch/Private/Animations/AN_SendTargetGroup.h
+++ Source/Crunch/Private/Animations/AN_SendTargetGroup.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameplayTagContainer.h"
 #include "Animation/AnimNotifies/AnimNotify.h"
+#include "GenericTeamAgentInterface.h"
 #include "AN_SendTargetGroup.generated.h"
 
 /**
@@ -18,8 +19,25 @@ public:
 	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
 
 private:
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	FGameplayTagContainer TriggerGameplayCueTags;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	TEnumAsByte<ETeamAttitude::Type> TargetTeam = ETeamAttitude::Hostile;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	float SphereSweepRadius = 60.f;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	bool bDrawDebug = true;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	bool bIgnoreOwner = true;
+	
 	UPROPERTY(EditAnywhere, Category="Gameplay Ability")
 	FGameplayTag EventTag;
 	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
 	TArray<FName> TargetSocketNames;
+
+	void SendLocalGameplayCue(const FHitResult& HitResult) const;
 };
