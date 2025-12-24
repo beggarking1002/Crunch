b15414e74b68bb0ab6ae81781d1b8c6a7067a08e
Beggarking1002 <drake1002@naver.com>
2025-12-24 13:12:00 +0900

    Implement the Inventory Drag and Drop Operator and Drag Drop Visual
    Implement the Inventory Drag and Drop Operator and Drag Drop Visual

---
+++ Source/Crunch/Private/Widgets/InventoryItemDragDropOp.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventoryItemDragDropOp.h"
+
+#include "InventoryItemWidget.h"
+#include "Blueprint/UserWidget.h"
+
+void UInventoryItemDragDropOp::SetDraggedItem(UInventoryItemWidget* DraggedItem)
+{
+	Payload = DraggedItem;
+	if (DragVisualClass)
+	{
+		UItemWidget* DragItemWidget = CreateWidget<UItemWidget>(GetWorld(), DragVisualClass);
+		if (DragItemWidget)
+		{
+			DragItemWidget->SetIcon(DraggedItem->GetIconTexture());
+			DefaultDragVisual = DragItemWidget;
+		}
+	}
+}
