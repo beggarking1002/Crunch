0720ffcd9bc0309b9d6411a19cdae865ec6c4fbe
Beggarking1002 <drake1002@naver.com>
2025-12-26 15:08:41 +0900

    Make the TreeNodeInterface and Implement It with ShopItemWidget
    Make the TreeNodeInterface and Implement It with ShopItemWidget

--- Source/Crunch/Private/Widgets/ShopItemWidget.h
+++ Source/Crunch/Private/Widgets/ShopItemWidget.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "ItemWidget.h"
+#include "TreeNodeInterface.h"
 #include "Blueprint/IUserObjectListEntry.h"
 #include "ShopItemWidget.generated.h"
 
@@ -16,18 +17,28 @@ DECLARE_MULTICAST_DELEGATE_OneParam(FOnShopItemSelected, const UShopItemWidget*)
  * 
  */
 UCLASS()
-class CRUNCH_API UShopItemWidget : public UItemWidget, public IUserObjectListEntry
+class CRUNCH_API UShopItemWidget : public UItemWidget, public IUserObjectListEntry, public ITreeNodeInterface
 {
 	GENERATED_BODY()
 public:
 	FOnItemPurchaseIssued OnItemPurchaseIssued;
 	FOnShopItemSelected OnShopItemClicked;
+
+	virtual UUserWidget* GetWidget() const override;
+	virtual TArray<const ITreeNodeInterface*> GetInputs() const override;
+	virtual TArray<const ITreeNodeInterface*> GetOutputs() const override;
+	virtual const UObject* GetItemObject() const override;
 	
 	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
 	FORCEINLINE const UPA_ShopItem* GetShopItem() const { return ShopItem; }
 private:
+	void CopyFromOther(const UShopItemWidget* OtherWidget);
+	void InitWithShopItem(const UPA_ShopItem* NewShopItem);
+	TArray<const ITreeNodeInterface*> ItemsToInterfaces(const TArray<const UPA_ShopItem*>& Items) const;
 	UPROPERTY()
 	const UPA_ShopItem* ShopItem;
+
+	const class UListView* ParentListView;
 	virtual void RightButtonClicked() override;
 	virtual void LeftButtonClicked() override;
 };
---
+++ Source/Crunch/Private/Widgets/TreeNodeInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TreeNodeInterface.h"
+
+
+// Add default functionality here for any ITreeNodeInterface functions that are not pure virtual.
