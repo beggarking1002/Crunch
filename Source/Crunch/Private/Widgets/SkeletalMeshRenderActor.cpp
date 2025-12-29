1ea24789b6da367743cbba5ca2ae305b80ef4a0c
Beggarking1002 <drake1002@naver.com>
2025-12-29 20:57:39 +0900

    Implement the Skeletal Mesh Renderer
    Implement the Skeletal Mesh Renderer

---
+++ Source/Crunch/Private/Widgets/SkeletalMeshRenderActor.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SkeletalMeshRenderActor.h"
+
+
+// Sets default values
+ASkeletalMeshRenderActor::ASkeletalMeshRenderActor()
+{
+	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh Comp");
+	MeshComp->SetupAttachment(GetRootComponent());
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	MeshComp->SetLightingChannels(false, true, false);
+}
+
+void ASkeletalMeshRenderActor::ConfigureSkeletalMesh(USkeletalMesh* MeshAsset, TSubclassOf<UAnimInstance> AnimBlueprint)
+{
+	MeshComp->SetSkeletalMeshAsset(MeshAsset);
+	MeshComp->SetAnimInstanceClass(AnimBlueprint);
+}
+
+// Called when the game starts or when spawned
+void ASkeletalMeshRenderActor::BeginPlay()
+{
+	Super::BeginPlay();
+	MeshComp->SetVisibleInSceneCaptureOnly(true);
+}
+
