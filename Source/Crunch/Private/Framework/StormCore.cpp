630143ec95828ab209f6763da43a8f23a43a3186
Beggarking1002 <drake1002@naver.com>
2025-12-30 17:52:14 +0900

    Update the Team Count and Team Weight
    Update the Team Count and Team Weight

--- Source/Crunch/Private/Framework/StormCore.cpp
+++ Source/Crunch/Private/Framework/StormCore.cpp
@@ -3,12 +3,19 @@
 
 #include "Framework/StormCore.h"
 
+#include "GenericTeamAgentInterface.h"
+#include "Components/SphereComponent.h"
+
 // Sets default values
 AStormCore::AStormCore()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	InfluenceRange = CreateDefaultSubobject<USphereComponent>("Influence Range");
+	InfluenceRange->SetupAttachment(GetRootComponent());
 
+	InfluenceRange->OnComponentBeginOverlap.AddDynamic(this, &AStormCore::NewInfluenerInRange);
+	InfluenceRange->OnComponentEndOverlap.AddDynamic(this, &AStormCore::InfluencerLeftRange);
 }
 
 // Called when the game starts or when spawned
@@ -30,5 +37,66 @@ void AStormCore::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void AStormCore::NewInfluenerInRange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	IGenericTeamAgentInterface* OtherTeamInterface = Cast<IGenericTeamAgentInterface>(OtherActor);
+	if (OtherTeamInterface)
+	{
+		if (OtherTeamInterface->GetGenericTeamId().GetId() == 0)
+		{
+			TeamOneInfluncerCount++;
+		}
+		else if (OtherTeamInterface->GetGenericTeamId().GetId() == 1)
+		{
+			TeamTwoInfluncerCount++;
+		}
+		UpdateTeamWeight();
+	}
+}
+
+void AStormCore::InfluencerLeftRange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	IGenericTeamAgentInterface* OtherTeamInterface = Cast<IGenericTeamAgentInterface>(OtherActor);
+	if (OtherTeamInterface)
+	{
+		if (OtherTeamInterface->GetGenericTeamId().GetId() == 0)
+		{
+			TeamOneInfluncerCount--;
+			if(TeamOneInfluncerCount< 0)
+			{
+				TeamOneInfluncerCount = 0;
+			}
+		}
+		else if (OtherTeamInterface->GetGenericTeamId().GetId() == 1)
+		{
+			TeamTwoInfluncerCount--;
+			if(TeamTwoInfluncerCount< 0)
+			{
+				TeamTwoInfluncerCount = 0;
+			}
+		}
+		UpdateTeamWeight();
+	}
+}
+
+void AStormCore::UpdateTeamWeight()
+{
+	if (TeamOneInfluncerCount == TeamTwoInfluncerCount)
+	{
+		TeamWeight = 0.f;
+	}
+	else
+	{
+		float TeamOffset = TeamOneInfluncerCount - TeamTwoInfluncerCount;
+		float TeamTotal = TeamOneInfluncerCount + TeamTwoInfluncerCount;
+
+		TeamWeight = TeamOffset / TeamTotal;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Team One Count: %d, Team Two Count: %d, Weight: %f"), TeamOneInfluncerCount, TeamTwoInfluncerCount, TeamWeight);
 }
 
