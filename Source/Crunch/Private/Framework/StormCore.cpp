13438fe6c589acb19c4684c5de35fe7b4e4010e0
Beggarking1002 <drake1002@naver.com>
2025-12-30 20:16:18 +0900

    Move the Storm Core Based on Weight
    Move the Storm Core Based on Weight

--- Source/Crunch/Private/Framework/StormCore.cpp
+++ Source/Crunch/Private/Framework/StormCore.cpp
@@ -3,8 +3,10 @@
 
 #include "Framework/StormCore.h"
 
+#include "AIController.h"
 #include "GenericTeamAgentInterface.h"
 #include "Components/SphereComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 AStormCore::AStormCore()
@@ -23,6 +25,12 @@ void AStormCore::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AStormCore::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	OwnerAIC = Cast<AAIController>(NewController);
 }
 
 // Called every frame
@@ -98,5 +106,31 @@ void AStormCore::UpdateTeamWeight()
 	}
 
 	UE_LOG(LogTemp, Warning, TEXT("Team One Count: %d, Team Two Count: %d, Weight: %f"), TeamOneInfluncerCount, TeamTwoInfluncerCount, TeamWeight);
+	UpdateGoal();
+}
+
+void AStormCore::UpdateGoal()
+{
+	if (!HasAuthority())
+		return;
+
+	if (!OwnerAIC)
+		return;
+
+	if (!GetCharacterMovement())
+		return;
+
+	if (TeamWeight > 0)
+	{
+		OwnerAIC->MoveToActor(TeamOneGoal);
+	}
+	else
+	{
+		OwnerAIC->MoveToActor(TeamTwoGoal);
+	}
+
+	float Speed = MaxMoveSpeed * FMath::Abs(TeamWeight);
+
+	GetCharacterMovement()->MaxWalkSpeed = Speed;
 }
 
