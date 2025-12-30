65b70382e446d14319df65c5cf878236a5eae2a0
Beggarking1002 <drake1002@naver.com>
2025-12-30 12:32:04 +0900

    Setup the Headshot Render
    Setup the Headshot Render

---
+++ Source/Crunch/Private/Widgets/SkeletalMeshRenderWidget.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SkeletalMeshRenderWidget.h"
+
+#include "SkeletalMeshRenderActor.h"
+#include "GameFramework/Character.h"
+
+void USkeletalMeshRenderWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	ACharacter* PlayerCharacter = GetOwningPlayerPawn<ACharacter>();
+	if (PlayerCharacter && SkeletalMeshRenderActor)
+	{
+		SkeletalMeshRenderActor->ConfigureSkeletalMesh(PlayerCharacter->GetMesh()->GetSkeletalMeshAsset(), PlayerCharacter->GetMesh()->GetAnimClass());
+	}
+}
+
+void USkeletalMeshRenderWidget::SpawnRenderActor()
+{
+	if (!SkeletalMeshRenderActorClass)
+		return;
+
+	UWorld* World = GetWorld();
+	if (!World)
+		return;
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	SkeletalMeshRenderActor = World->SpawnActor<ASkeletalMeshRenderActor>(SkeletalMeshRenderActorClass, SpawnParams);
+}
+
+ARenderActor* USkeletalMeshRenderWidget::GetRenderActor() const
+{
+	return SkeletalMeshRenderActor;
+}
