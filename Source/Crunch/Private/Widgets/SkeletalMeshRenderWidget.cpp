3662d11bd382581fb3158607f7d40bfd487824e0
Beggarking1002 <drake1002@naver.com>
2026-01-05 15:24:05 +0900

    Define the Headshot Capture Local Position and Rotation
    Define the Headshot Capture Local Position and Rotation

--- Source/Crunch/Private/Widgets/SkeletalMeshRenderWidget.cpp
+++ Source/Crunch/Private/Widgets/SkeletalMeshRenderWidget.cpp
@@ -3,16 +3,25 @@
 
 #include "SkeletalMeshRenderWidget.h"
 
+#include "RenderActorTargetInterface.h"
 #include "SkeletalMeshRenderActor.h"
+#include "Components/SceneCaptureComponent2D.h"
 #include "GameFramework/Character.h"
 
 void USkeletalMeshRenderWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 	ACharacter* PlayerCharacter = GetOwningPlayerPawn<ACharacter>();
+	IRenderActorTargetInterface* PlayerCharacterRenderTargetInterface = Cast<IRenderActorTargetInterface>(PlayerCharacter);
 	if (PlayerCharacter && SkeletalMeshRenderActor)
 	{
 		SkeletalMeshRenderActor->ConfigureSkeletalMesh(PlayerCharacter->GetMesh()->GetSkeletalMeshAsset(), PlayerCharacter->GetMesh()->GetAnimClass());
+		USceneCaptureComponent2D* SceneCapture = SkeletalMeshRenderActor->GetCaptureComponent();
+		if (PlayerCharacterRenderTargetInterface && SceneCapture)
+		{
+			SceneCapture->SetRelativeLocation(PlayerCharacterRenderTargetInterface->GetCaptureLocalPosition());
+			SceneCapture->SetRelativeRotation(PlayerCharacterRenderTargetInterface->GetCaptureLocalRotation());
+		}
 	}
 }
 
