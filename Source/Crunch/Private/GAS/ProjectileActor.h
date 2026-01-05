ce6a34d51a12ae641505a7f141369f2e79cc50d5
Beggarking1002 <drake1002@naver.com>
2026-01-05 10:31:05 +0900

    Apply the Hit Effect and Fire the Hit Effect
    Apply the Hit Effect and Fire the Hit Effect

--- Source/Crunch/Private/GAS/ProjectileActor.h
+++ Source/Crunch/Private/GAS/ProjectileActor.h
@@ -30,7 +30,12 @@ public:
 	
 	/** Retrieve team identifier in form of FGenericTeamId */
 	virtual FGenericTeamId GetGenericTeamId() const { return TeamId; }
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
 private:
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay Cue")
+	FGameplayTag HitGameplayCueTag;
+	
 	UPROPERTY(Replicated)
 	FGenericTeamId TeamId;
 
@@ -54,6 +59,10 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+
+private:
 	void TravelMaxDIstanceReached();
+	void SendLocalGameplayCue(AActor* CueTargetActor, const FHitResult& HitResult);
+	
 
 };
