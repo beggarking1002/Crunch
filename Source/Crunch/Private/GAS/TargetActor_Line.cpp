26169814fa795bebc8898aa92c060c2c45720afb
Beggarking1002 <drake1002@naver.com>
2026-01-06 14:43:07 +0900

    Create the Lazer Target Actor
    Create the Lazer Target Actor

---
+++ Source/Crunch/Private/GAS/TargetActor_Line.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TargetActor_Line.h"
+#include "NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+#include "Crunch/Crunch.h"
+
+
+ATargetActor_Line::ATargetActor_Line()
+{
+	RootComp = CreateDefaultSubobject<USceneComponent>("Root Comp");
+	SetRootComponent(RootComp);
+
+	TargetEndDetectionSphere = CreateDefaultSubobject<USphereComponent>("Target End Detection Sphere");
+	TargetEndDetectionSphere->SetupAttachment(GetRootComponent());
+	TargetEndDetectionSphere->SetCollisionResponseToChannel(ECC_SpringArm, ECR_Ignore);
+
+	LazerVFX = CreateDefaultSubobject<UNiagaraComponent>("Lazer VFX");
+	LazerVFX->SetupAttachment(GetRootComponent());
+
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+	ShouldProduceTargetDataOnServer = true;
+
+	AvatarActor = nullptr;
+}
+
