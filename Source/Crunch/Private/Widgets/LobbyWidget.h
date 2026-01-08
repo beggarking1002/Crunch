bfeec4c192bbee290cada100b24433d9a591599e
Beggarking1002 <drake1002@naver.com>
2026-01-08 18:39:40 +0900

    Create The Lobby Classes
    Create The Lobby Classes

---
+++ Source/Crunch/Private/Widgets/LobbyWidget.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LobbyWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API ULobbyWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:	
+	UPROPERTY(meta=(BindWidget))
+	class UWidgetSwitcher* MainSwitcher;
+
+	UPROPERTY(meta=(BindWidget))	
+	class UWidget* TeamSelectionRoot;
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* StartHeroSelectionButton;
+
+	UPROPERTY(meta=(BindWidget))
+	class UUniformGridPanel* TeamSelectionSlotGridPanel;
+};
