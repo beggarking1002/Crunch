9a7e18ccef516cc3ef160fab587e723a48253c72
Beggarking1002 <drake1002@naver.com>
2026-01-09 13:04:59 +0900

    Create the Team Selection Slot Widget
    Create the Team Selection Slot Widget

---
+++ Source/Crunch/Private/Widgets/TeamSelectionWidget.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TeamSelectionWidget.h"
+
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+
+void UTeamSelectionWidget::SetSlotID(uint8 NewSlotID)
+{
+	SlotID = NewSlotID;
+}
+
+void UTeamSelectionWidget::UpdateSlotInfo(const FString& PlayerNickName)
+{
+	InfoText->SetText(FText::FromString(PlayerNickName));
+}
+
+void UTeamSelectionWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	SelectButton->OnClicked.AddDynamic(this, &UTeamSelectionWidget::SelectButtonClicked);
+}
+
+void UTeamSelectionWidget::SelectButtonClicked()
+{
+	OnSlotClicked.Broadcast(SlotID);
+}
