1fda73381ece82ccacdbc5c5aa7aeb3e284188d8
Beggarking1002 <drake1002@naver.com>
2026-01-10 16:00:02 +0900

    Add the Character Definition Primary Asset
    Add the Character Definition Primary Asset

---
+++ Source/Crunch/Private/Character/PA_CharacterDefinition.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PA_CharacterDefinition.h"
+
+#include "CCharacter.h"
+
+FPrimaryAssetId UPA_CharacterDefinition::GetPrimaryAssetId() const
+{
+	return FPrimaryAssetId(GetCharacterDefinitionAssetType(), GetFName());
+}
+
+FPrimaryAssetType UPA_CharacterDefinition::GetCharacterDefinitionAssetType()
+{
+	return FPrimaryAssetType("CharacterDefinition");
+}
+
+UTexture2D* UPA_CharacterDefinition::LoadIcon() const
+{
+	CharacterIcon.LoadSynchronous();
+	if (CharacterIcon.IsValid())
+		return CharacterIcon.Get();
+
+	return nullptr;
+}
+
+TSubclassOf<ACCharacter> UPA_CharacterDefinition::LoadCharacterClass() const
+{
+	CharacterClass.LoadSynchronous();
+	if (CharacterClass.IsValid())
+		return CharacterClass.Get();
+	
+	return TSubclassOf<ACCharacter>();
+
+}
+
+TSubclassOf<UAnimInstance> UPA_CharacterDefinition::LoadDisplayAnimationBP() const
+{
+	DisplayAnimBP.LoadSynchronous();
+	if (DisplayAnimBP.IsValid())
+		return DisplayAnimBP.Get();
+
+	return TSubclassOf<UAnimInstance>();
+}
+
+USkeletalMesh* UPA_CharacterDefinition::LoadDisplayMesh() const
+{
+	TSubclassOf<ACCharacter> LoadedCharaterClass = LoadCharacterClass();
+	if (!LoadedCharaterClass)
+		return nullptr;
+
+	ACharacter* Character = Cast<ACharacter>(LoadedCharaterClass.GetDefaultObject());
+	if (!Character)
+		return nullptr;
+
+	return Character->GetMesh()->GetSkeletalMeshAsset();
+}
