0aa218bf85158d31a738978031556f3ca1b1ea1a
Beggarking1002 <drake1002@naver.com>
2026-01-11 21:46:21 +0900

    Create the Character Display Class
    Create the Character Display Class

---
+++ Source/Crunch/Private/Widgets/CharacterDisplay.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterDisplay.h"
+
+#include "Camera/CameraComponent.h"
+#include "Character/PA_CharacterDefinition.h"
+
+
+// Sets default values
+ACharacterDisplay::ACharacterDisplay()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = true;
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("Root Comp"));
+
+	MeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh Component");
+	MeshComponent->SetupAttachment(GetRootComponent());
+	
+	ViewCameraComponent = CreateDefaultSubobject<UCameraComponent>("View Camera Component");
+	ViewCameraComponent->SetupAttachment(GetRootComponent());
+}
+
+void ACharacterDisplay::ConfigureWithCharacterDefinition(const UPA_CharacterDefinition* CharacterDefinition)
+{
+	if (!CharacterDefinition)
+		return;
+
+	MeshComponent->SetSkeletalMesh(CharacterDefinition->LoadDisplayMesh());
+	MeshComponent->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+
+}
+
+
+
---
+++ Source/Crunch/Private/Widgets/CharacterDisplay.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CharacterDisplay.generated.h"
+
+class UPA_CharacterDefinition;
+
+UCLASS()
+class CRUNCH_API ACharacterDisplay : public AActor
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this actor's properties
+	ACharacterDisplay();
+	void ConfigureWithCharacterDefinition(const UPA_CharacterDefinition* CharacterDefinition);
+
+private:	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Character Display")
+	class USkeletalMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Character Display")
+	class UCameraComponent* ViewCameraComponent;
+};
