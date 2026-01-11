132f2d54e855b5313f24548d2f0c44f7c9161323
Beggarking1002 <drake1002@naver.com>
2026-01-11 18:47:31 +0900

    Add the Player Selection and Character Selection Server Function in the Player State
    Add the Player Selection and Character Selection Server Function in the Player State

--- Source/Crunch/Private/Player/CPlayerState.cpp
+++ Source/Crunch/Private/Player/CPlayerState.cpp
@@ -2,3 +2,68 @@
 
 
 #include "CPlayerState.h"
+
+#include "Framework/CGameState.h"
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+
+ACPlayerState::ACPlayerState()
+{
+	bReplicates = true;
+	NetUpdateFrequency = 100.f;
+}
+
+void ACPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ACPlayerState, PlayerSelection);
+}
+
+void ACPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+	CGameState = Cast<ACGameState>(UGameplayStatics::GetGameState(this));
+
+	if (CGameState)
+	{
+		CGameState->OnPlayerSelectionUpdated.AddUObject(this, &ACPlayerState::PlayerSelectionUpdated);
+	}
+	
+}
+
+void ACPlayerState::Server_SetSelectedCharacterDefinition_Implementation(const UPA_CharacterDefinition* NewDefinition)
+{
+	if (!CGameState)
+		return;
+
+	if (!NewDefinition)
+		return;
+
+	if (CGameState->IsDefinitionSelected(NewDefinition))
+		return;
+
+	if (PlayerSelection.GetCharacterDefinition())
+	{
+		CGameState->SetCharacterDeselected(PlayerSelection.GetCharacterDefinition());
+	}
+
+	PlayerSelection.SetCharacterDefinition(NewDefinition);
+
+}
+
+bool ACPlayerState::Server_SetSelectedCharacterDefinition_Validate(const UPA_CharacterDefinition* NewDefinition)
+{
+	return true;
+
+}
+
+void ACPlayerState::PlayerSelectionUpdated(const TArray<FPlayerSelection>& NewPlayerSelections)
+{
+	for (const FPlayerSelection& NewPlayerSelection : NewPlayerSelections)
+	{
+		if (NewPlayerSelection.IsForPlayer(this))
+		{
+			PlayerSelection = NewPlayerSelection;
+		}
+	}
+}
--- Source/Crunch/Private/Player/CPlayerState.h
+++ Source/Crunch/Private/Player/CPlayerState.h
@@ -3,9 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PlayerInfoTypes.h"
 #include "GameFramework/PlayerState.h"
 #include "CPlayerState.generated.h"
 
+class UPA_CharacterDefinition;
 /**
  * 
  */
@@ -13,4 +15,20 @@ UCLASS()
 class CRUNCH_API ACPlayerState : public APlayerState
 {
 	GENERATED_BODY()
+
+public:
+	ACPlayerState();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void BeginPlay() override;
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetSelectedCharacterDefinition(const UPA_CharacterDefinition* NewDefinition);
+
+private:
+	UPROPERTY(Replicated)
+	FPlayerSelection PlayerSelection;
+
+	UPROPERTY()
+	class ACGameState* CGameState;
+
+	void PlayerSelectionUpdated(const TArray<FPlayerSelection>& NewPlayerSelections);
 };
