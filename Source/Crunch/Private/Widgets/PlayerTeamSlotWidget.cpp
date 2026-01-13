b73a92d054f84633feed27077f78f02f0aeecb69
Beggarking1002 <drake1002@naver.com>
2026-01-13 19:59:03 +0900

    Implement the Player Team Slot Widget
    Implement the Player Team Slot Widget

---
+++ Source/Crunch/Private/Widgets/PlayerTeamSlotWidget.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerTeamSlotWidget.h"
+#include "Character/PA_CharacterDefinition.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void UPlayerTeamSlotWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	PlayerCharacterIcon->GetDynamicMaterial()->SetScalarParameterValue(CharacterEmptyMatParamName, 1);
+	CachedCharacterNameStr = "";
+}
+
+void UPlayerTeamSlotWidget::UpdateSlot(const FString& PlayerName, const UPA_CharacterDefinition* CharacterDefinition)
+{
+	CachedPlayerNameStr = PlayerName;
+
+	if (CharacterDefinition)
+	{
+		PlayerCharacterIcon->GetDynamicMaterial()->SetTextureParameterValue(CharacterIconMatParamName, CharacterDefinition->LoadIcon());
+		PlayerCharacterIcon->GetDynamicMaterial()->SetScalarParameterValue(CharacterEmptyMatParamName, 0);
+		CachedCharacterNameStr = CharacterDefinition->GetCharacterDisplayName();
+	}
+	else
+	{
+		PlayerCharacterIcon->GetDynamicMaterial()->SetScalarParameterValue(CharacterEmptyMatParamName, 1);
+		CachedCharacterNameStr = "";
+	}
+
+	UpdateNameText();
+}
+
+void UPlayerTeamSlotWidget::NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseEnter(InGeometry, InMouseEvent);
+	NameText->SetText(FText::FromString(CachedCharacterNameStr));
+	PlayAnimationForward(HoverAnim);
+}
+
+void UPlayerTeamSlotWidget::NativeOnMouseLeave(const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseLeave(InMouseEvent);
+	NameText->SetText(FText::FromString(CachedPlayerNameStr));
+	PlayAnimationReverse(HoverAnim);
+}
+
+void UPlayerTeamSlotWidget::UpdateNameText()
+{
+	if (IsHovered())
+	{
+		NameText->SetText(FText::FromString(CachedCharacterNameStr));
+	}
+	else
+	{
+		NameText->SetText(FText::FromString(CachedPlayerNameStr));
+	}
+}
+
+
