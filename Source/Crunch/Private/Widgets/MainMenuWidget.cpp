2aab2425db89e39d5ef3bfe033c20f3372222869
Beggarking1002 <drake1002@naver.com>
2026-01-28 23:00:20 +0900

    Create the Login Widgets
    Create the Login Widgets

--- Source/Crunch/Private/Widgets/MainMenuWidget.cpp
+++ Source/Crunch/Private/Widgets/MainMenuWidget.cpp
@@ -2,3 +2,35 @@
 
 
 #include "MainMenuWidget.h"
+#include "Framework/CGameInstance.h"
+#include "Components/Button.h"
+
+void UMainMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	CGameInstance = GetGameInstance<UCGameInstance>();
+	if (CGameInstance)
+	{
+
+	}
+
+	LoginBtn->OnClicked.AddDynamic(this, &UMainMenuWidget::LoginBtnClicked);
+}
+
+void UMainMenuWidget::LoginBtnClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Logining In!"))
+}
+
+void UMainMenuWidget::LoginCompleted(bool bWasSuccessful, const FString& PlayerNickname, const FString& ErrorMsg)
+{
+	if (bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Login successful"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Login Failed!"))
+	}
+}
--- Source/Crunch/Private/Widgets/MainMenuWidget.h
+++ Source/Crunch/Private/Widgets/MainMenuWidget.h
@@ -13,4 +13,33 @@ UCLASS()
 class CRUNCH_API UMainMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+
+	/******************************/	
+	/*           Main             */
+	/******************************/
+private:
+	UPROPERTY(meta=(BindWidget))
+	class UWidgetSwitcher* MainSwitcher;
+
+	UPROPERTY()
+	class UCGameInstance* CGameInstance;
+
+	/******************************/	
+	/*           Main             */
+	/******************************/
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	class UWidget* LoginWidgetRoot;
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* LoginBtn;
+
+	UFUNCTION()
+	void LoginBtnClicked();
+
+	void LoginCompleted(bool bWasSuccessful, const FString& PlayerNickname, const FString& ErrorMsg);
 };
