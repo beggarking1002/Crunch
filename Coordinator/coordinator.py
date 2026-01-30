a652482f4b0bde57e5712b787874e4592129e6ab
Beggarking1002 <drake1002@naver.com>
2026-01-30 11:49:49 +0900

    Using the HTTP server to Launch and Instance of the Server Upon Request
    Using the HTTP server to Launch and Instance of the Server Upon Request

--- Coordinator/coordinator.py
+++ Coordinator/coordinator.py
@@ -8,11 +8,31 @@ app=Flask(__name__)
 # TODO: Remove when using docker in the future
 nextAvailablePort = 7777
 
+def CreateServerLocalTest(sessionName, sessionSearchId):
+    global nextAvailablePort
+    subprocess.Popen([
+        "C:/UnrealSrc/UnrealEngine/Engine/Binaries/Win64/UnrealEditor.exe",
+        "C:/UE5/Crunch/Crunch.uproject",
+        "-server",
+        "-log",
+        '-epicapp="ServerClient"',
+        f'-SESSION_NAME="{sessionName}"',
+        f'-SESSION_SEARCH_ID="{sessionSearchId}"',
+        f'-PORT={nextAvailablePort}'
+    ])
+
+    usedPort = nextAvailablePort
+    nextAvailablePort += 1
+    return usedPort
+
 @app.route('/Sessions', methods=['POST'])
 def CreateServer():
     print(dict(request.headers))
 
-    port = nextAvailablePort
+    sessionName = request.get_json().get(SESSION_NAME_KEY)
+    sessionSearchId = request.get_json().get(SESSION_SEARCH_ID_KEY)
+
+    port = CreateServerLocalTest(sessionName, sessionSearchId)
     return jsonify({"status": "success", PORT_KEY: port}), 200
 
 if __name__=="__main__":
