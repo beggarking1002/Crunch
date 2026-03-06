1536c39759e436c9833f25159e496e44ea8b2dc8
Beggarking1002 <drake1002@naver.com>
2026-03-06 16:05:47 +0900

    Finish and Test the Coordinator Container
    Finish and Test the Coordinator Container

--- ServerDeploy/coordinator/coordinator/coordinator.py
+++ ServerDeploy/coordinator/coordinator/coordinator.py
@@ -1,3 +1,4 @@
+#include the Flask, request, and jsonify modules from the flask libraray
 from flask import Flask, request, jsonify
 import subprocess
 from consts import SESSION_NAME_KEY, SESSION_SEARCH_ID_KEY, PORT_KEY
@@ -8,7 +9,7 @@ app=Flask(__name__)
 def GetUsedPorts():
     result = subprocess.run(['docker', 'ps', '--format', '"{{.Ports}}"'], capture_output=True, text=True)
     output = result.stdout
-    
+
     usedPorts = set()
 
     for line in output.strip().split("\n"):
@@ -17,15 +18,39 @@ def GetUsedPorts():
     
     return usedPorts
     
-def CreateServerImpl(sesssionName, sessionSearchId):
-    ports = GetUsedPorts()
-    print(ports)
+def FindNextAvailablePort(start=7777, end=8000):
+    usedPorts = GetUsedPorts()
+    for port in range(start, end+1):
+        if port not in usedPorts:
+            return port
+    
+    return 0
 
+def CreateServerImpl(sessionName, sessionSearchId):
+    port = FindNextAvailablePort()
+    print(f"Launching server: {sessionName}, with id: {sessionSearchId}, at port: {port}")
+
+    subprocess.Popen([
+        "docker",
+        "run",
+        "--rm",
+        "-p", f"{port}:{port}/tcp",
+        "-p", f"{port}:{port}/udp",
+        "crunchserver",
+        "-server",
+        "-log",
+        '-epicapp="ServerClient"',
+        f'-SESSION_NAME="{sessionName}"',
+        f'-SESSION_SEARCH_ID="{sessionSearchId}"',
+        f'-PORT={port}'
+    ])
+
+    return port
 
 # TODO: Remove when using docker in the future
 nextAvailablePort = 7777
 
-def CreateServerLocalTest(sessionName, sessionSearchId):
+def CreateSeverLocalTest(sessionName, sessionSearchId):
     global nextAvailablePort
     subprocess.Popen([
         "C:/UnrealSrc/UnrealEngine/Engine/Binaries/Win64/UnrealEditor.exe",
@@ -42,17 +67,16 @@ def CreateServerLocalTest(sessionName, sessionSearchId):
     nextAvailablePort += 1
     return usedPort
 
+
 @app.route('/Sessions', methods=['POST'])
-def CreateServer():
+def CreateSever():
     print(dict(request.headers))
 
     sessionName = request.get_json().get(SESSION_NAME_KEY)
     sessionSearchId = request.get_json().get(SESSION_SEARCH_ID_KEY)
 
-    port = CreateServerLocalTest(sessionName, sessionSearchId)
+    port = CreateServerImpl(sessionName, sessionSearchId)
     return jsonify({"status": "success", PORT_KEY: port}), 200
 
-if __name__=="__main__":
-    #app.run(host="0.0.0.0", port=5000)
-    CreateServerImpl("","")
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
